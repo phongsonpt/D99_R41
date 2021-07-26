2ccec95fbf9c5a75b47a3f0688cd421c5534584a
phongsonpt <87954813+phongsonpt@users.noreply.github.com>
2021-07-26 08:56:09 +0700

    Add files via upload

---
+++ D99D0241/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("LEMON3 (R)")> 
+<Assembly: AssemblyDescription("General")> 
+<Assembly: AssemblyCompany("DigiNet Corporation")> 
+<Assembly: AssemblyProduct("DigiNet Corporation")> 
+<Assembly: AssemblyCopyright("(C) 2000 - 2005 DigiNet Corporation")> 
+<Assembly: AssemblyTrademark("LEMON3 (R)")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("31903d85-7549-4468-b3c3-3e3097fe3d46")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("4.10.0.0")> 
+<Assembly: AssemblyFileVersion("4.10.0.0")> 
